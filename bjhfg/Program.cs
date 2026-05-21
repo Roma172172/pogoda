36319127030bd7c94b53df84b073205394d21e16
RR <roman79220715046@gmail.com>
2026-05-21 19:50:13 +0500

    .

--- bjhfg/Program.cs
+++ bjhfg/Program.cs
@@ -1,27 +1,21 @@
-﻿using System;
 using Avalonia;
-using ReactiveUI.Avalonia;
+using System;
 
-namespace bjhfg
+namespace WeatherAppAvalonia;
+
+// Точка входа в приложение
+internal sealed class Program
 {
-    internal sealed class Program
+    [STAThread]
+    public static void Main(string[] args)
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
+        BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-#if DEBUG
-                .WithDeveloperTools()
-#endif
-                .WithInterFont()
-                .LogToTrace()
-                .UseReactiveUI();
     }
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
